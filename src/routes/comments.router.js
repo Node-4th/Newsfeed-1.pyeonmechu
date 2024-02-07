908c456ef1999ff386b65f9cc97690e68f1c9521
slianzg <slian201248@gmail.com>
2024-02-07 19:59:32 +0900

    ✨ feat: 댓글 수정 API 추가

--- src/routes/comments.router.js
+++ src/routes/comments.router.js
@@ -85,11 +85,69 @@ router.get("/posts/:postId/comments", async (req, res, next) => {
   return res.status(200).json({ data: comments });
 });
 
-//댓글 수정 APi
+//댓글 수정 API
 
 router.patch(
   "/posts/:postId/comments/:commentId",
-  async (req, res, next) => {}
+  authMiddleware,
+  async (req, res, next) => {
+    const { postId, commentId } = req.params;
+    const user = req.user;
+    const { content } = req.body;
+
+    if (!postId) {
+      return res.status(400).json({
+        success: false,
+        message: "postId는 필수로 입력되어야 합니다.",
+      });
+    }
+
+    if (!commentId) {
+      return res.status(400).json({
+        success: false,
+        message: "commentId는 필수로 입력되어야 합니다.",
+      });
+    }
+
+    if (!content) {
+      return res.status(404).json({
+        success: false,
+        message: "수정할 내용을 입력해주세요.",
+      });
+    }
+
+    const post = await prisma.posts.findFirst({ where: { postId: +postId } });
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ success: false, message: "해당 게시물을 찾을 수 없습니다." });
+    }
+
+    const comment = await prisma.comments.findFirst({
+      where: { commentId: +commentId },
+    });
+
+    if (!comment) {
+      return res
+        .status(404)
+        .json({ success: false, message: "해당 댓글을 찾을 수 없습니다." });
+    }
+
+    if (user.grade === "USER" && user.userId !== comment.userId) {
+      return res.status(401).json({
+        success: false,
+        message: "댓글을 수정할 권한이 없습니다.",
+      });
+    }
+
+    await prisma.comments.update({
+      where: { commentId: +commentId },
+      data: { content: content },
+    });
+
+    return res.status(200).json({ message: "댓글 수정사항이 저장되었습니다." });
+  }
 );
 
 //댓글 삭제 API
