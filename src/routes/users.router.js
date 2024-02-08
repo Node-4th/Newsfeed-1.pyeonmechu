4edc98e936b43ba4acfb501d934b708c23e40277
jacoder222 <bjuop222@gmail.com>
2024-02-08 20:00:55 +0900

    :hammer: 프로필 API수정

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -173,47 +173,39 @@ router.get("/users/me", authMiddleware, async (req, res, next) => {
 //프로필 수정
 router.patch("/users/me", authMiddleware, async (req, res, next) => {
   try {
-    const {
-      email,
-      password,
-      passwordConfirm,
-      name,
-      nickname,
-      profileImage,
-      aboutMe,
-    } = req.body;
+    const { email, password, name, nickname, profileImage, aboutMe } = req.body;
     const { userId } = req.user;
 
     const user = await prisma.users.findFirst({
       where: { userId: +userId },
     });
 
-    if (!user && user.grade === "USER")
-      return res.status(401).json({ message: "수정할 권한이 없습니다." });
-    if (!email)
-      return res.status(401).json({ message: "이미엘은 필수값입니다." });
-    if (!password)
-      return res.status(401).json({ message: "비밀번호는 필수값입니다." });
-    if (password !== passwordConfirm)
-      return res
-        .status(401)
-        .json({ message: "비밀번호와 비밀번호 확인이 일치하지 않습니다." });
-    if (name) return res.status(401).json({ message: "이름은 필수값입니다." });
+    if (!user)
+      return res.status(404).json({ message: "프로필이 존재하지 않습니다." });
+    if (user.userId !== +userId && user.grade === "USER")
+      return res.status(401).json({ message: "자신의 프로필이 아닙니다." });
+
+    const updateEmail = email ? email : user.email;
+    const updatePassword = password ? password : user.password;
+    const updateName = name ? name : user.name;
+    const updateNickName = nickname ? nickname : user.nickname;
+    const updateProfileImage = profileImage ? profileImage : user.profileImage;
+    const updateAboutMe = aboutMe ? aboutMe : user.aboutMe;
 
     await prisma.users.update({
       where: { userId: +userId },
       data: {
-        email,
-        password,
-        name,
-        nickname,
-        profileImage,
-        aboutMe,
+        email: updateEmail,
+        password: updatePassword,
+        name: updateName,
+        nickname: updateNickName,
+        profileImage: updateProfileImage,
+        aboutMe: updateAboutMe,
       },
     });
 
     return res
-      .status(200)
+      .status(201)
       .json({ success: true, message: "프로필 수정이 완료되었습니다." });
   } catch (err) {
     next(err);
@@ -224,13 +216,6 @@ router.patch("/users/me", authMiddleware, async (req, res, next) => {
 router.delete("/users/leave", authMiddleware, async (req, res, next) => {
   try {
     const { userId } = req.user;
-
-    const user = await prisma.users.findFirst({
-      where: { userId: +userId },
-    });
-
-    if (!user)
-      return res.status(404).json({ message: "찾는 사용자가 없습니다.." });
 
     await prisma.users.delete({
       where: { userId: +userId },
@@ -245,47 +230,44 @@ router.delete("/users/leave", authMiddleware, async (req, res, next) => {
 });
 
 // 다른 사람 포스트 보기
-router.get("/users/:userId", authMiddleware, async (req, res, next) => {
+router.get("/users/:userId", async (req, res, next) => {
   try {
-    const { params } = req.params;
-    const { userId } = req.user;
+    const { userId } = req.params;
 
     const othersPost = await prisma.posts.findMany({
-      where: { postId },
+      where: { postId: +userId },
     });
 
-    if (othersPost !== params)
+    if (!othersPost)
       return res.status(404).json({ message: "존재하지 않는 글입니다." });
 
-    if (othersPost.userId !== userId) {
-      const otherPost = await prisma.posts.findMany({
-        where: { userId: +params },
-        select: {
-          userId: true,
-          title: true,
-          content: true,
-          imageURL: true,
-          tag: true,
-          category: true,
-          like: true,
-          unlike: true,
-          createdAt: true,
-          updatedAt: true,
-          users: {
-            select: {
-              userId: true,
-              email: true,
-              mame: true,
-              nickname: true,
-              profileImage: true,
-              aboutMe: true,
-            },
+    const otherPost = await prisma.posts.findMany({
+      where: { postId: +userId },
+      select: {
+        userId: true,
+        title: true,
+        content: true,
+        imageURL: true,
+        tag: true,
+        category: true,
+        like: true,
+        unlike: true,
+        createdAt: true,
+        updatedAt: true,
+        users: {
+          select: {
+            userId: true,
+            email: true,
+            mame: true,
+            nickname: true,
+            profileImage: true,
+            aboutMe: true,
           },
         },
-      });
+      },
+    });
 
-      return res.status(200).json({ data: otherPost });
-    }
+    return res.status(200).json({ data: otherPost });
   } catch (err) {
     next(err);
   }
