3f01fe294ab60a1d10aa1498192d4c3151d02a4b
wanhee27 <chowh727@gmail.com>
2024-02-08 13:02:11 +0900

    :recycle: http상태코드 오타 변수명 비밀번호길이제한 수정

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { prisma } from "../utils/index.js";
 import bcrypt from "bcrypt";
-import { generateToken, generateRefreshToken } from "../utils/jwt.js";
+import { AccessToken, RefreshToken } from "../utils/jwt.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
@@ -12,6 +12,7 @@ router.post("/sign-up", async (req, res, next) => {
     const {
       email,
       password,
+      passwordConfirm,
       name,
       nickname,
       profileImage,
@@ -26,53 +27,59 @@ router.post("/sign-up", async (req, res, next) => {
     }
     if (!email) {
       return res
-        .status(404)
-        .json({ success: false, message: "이메일을 입력해주세요" }); // 404 - Not Found (찾을 수 없음)
+        .status(400)
+        .json({ success: false, message: "이메일을 입력해주세요" });
     }
     // 비밀번호 존재 유무
     if (!password) {
       return res
-        .status(404)
-        .json({ success: false, message: "비밀번호를 입력해주세요" }); // 404 - Not Found (찾을 수 없음)
+        .status(400)
+        .json({ success: false, message: "비밀번호를 입력해주세요" });
     }
     // 비밀번호의 길이
-    if (password.length < 6) {
+    if (password.length < 6 || password.length > 20) {
       return res
         .status(400)
-        .json({ success: false, message: "비밀번호는 최소 6자리 이상입니다." }); // 400 - Bad Request (잘못된요청)
+        .json({
+          success: false,
+          message: "비밀번호는 최소 6자리 이상, 최대 20자리 이하 입니다.",
+        });
+    }
+    // 비밀번호 확인
+    if (password !== passwordConfirm) {
+      return res.status(400).json({ message: "비밀번호가 일치하지 않습니다." });
     }
     // 이름 존재 유무
     if (!name) {
       return res
-        .status(404)
-        .json({ success: false, message: "이름을 입력해주세요" }); // 404 - Not Found (찾을 수 없음)
+        .status(400)
+        .json({ success: false, message: "이름을 입력해주세요" });
     }
     // 닉네임 존재 유무
     if (!nickname) {
       return res
-        .status(404)
-        .json({ success: false, message: "닉네임을 입력해주세요." }); // 404 - Not Found (찾을 수 없음)
+        .status(400)
+        .json({ success: false, message: "닉네임을 입력해주세요." });
     }
     // // 프로필이미지 존재 유무
     // if (!profileImage) {
     //   return res
     //     .status(404)
-    //     .json({ success: false, message: "프로필 이미지를 등록해주세요." }); // 404 - Not Found (찾을 수 없음)
+    //     .json({ success: false, message: "프로필 이미지를 등록해주세요." });
     // }
     // // 자기소개 존재 유무
     // if (!aboutMe) {
     //   return res
     //     .status(404)
-    //     .json({ success: false, message: "자기소개를 작성해주세요." }); // 404 - Not Found (찾을 수 없음)
-    // }
+    //     .json({ success: false, message: "자기소개를 작성해주세요." });
     // 이메일 확인
-    const isExisUser = await prisma.users.findFirst({
+    const isExistUser = await prisma.users.findFirst({
       where: { email },
     });
     // 이메일 중복 여부
-    if (isExisUser) {
+    if (isExistUser) {
       return res
-        .status(400)
+        .status(409)
         .json({ success: false, message: "이미 가입된 이메일입니다." });
     }
     // 패스워드 복호화
@@ -105,23 +112,23 @@ router.post("/sign-in", async (req, res, next) => {
     }
     // 비밀번호 유무 확인
     if (!password) {
-      return res.status(404).json({ message: "비밀번호를 입력해주세요" });
+      return res.status(400).json({ message: "비밀번호를 입력해주세요" });
     }
     // 사용자 조회
     const user = await prisma.users.findFirst({ where: { email } });
     // 사용자 존재 여부 확인
     if (!user) {
-      return res.status(404).json({ message: "존재하지 않는 이메일입니다." });
+      return res.status(400).json({ message: "존재하지 않는 이메일입니다." });
     }
     // 비밀번호 일치 여부 확인
     if (!(await bcrypt.compare(password, user.password))) {
       return res.status(400).json({ message: "비밀번호가 일치하지 않습니다." });
     }
     // 쿠키 할당 및 출력
-    const token = generateToken({ userId: user.userId });
-    const refreshToken = generateRefreshToken({ userId: user.userId });
+    const token = AccessToken({ userId: user.userId });
+    // const refreshToken = RefreshToken({ userId: user.userId });
     res.cookie("authorization", `Bearer ${token}`);
-    res.cookie("refreshToken", `Bearer ${refreshToken}`);
+    // res.cookie("refreshToken", `Bearer ${refreshToken}`);
     return res.status(200).json({ message: "로그인에 성공하였습니다." });
   } catch (error) {
     next(error);
@@ -133,7 +140,7 @@ router.post("/logout", async (req, res, next) => {
   try {
     // 쿠키를 삭제하여 로그아웃
     res.clearCookie("authorization");
-    res.clearCookie("refreshToken");
+    // res.clearCookie("refreshToken");
 
     return res.status(200).json({ message: "로그아웃 되었습니다." });
   } catch (error) {
