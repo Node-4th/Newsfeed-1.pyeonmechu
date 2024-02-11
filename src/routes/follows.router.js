6a0b61d1ebb35dab56bfd6bc3db98f26655c492e
slianzg <slian201248@gmail.com>
2024-02-12 01:37:42 +0900

    ✨ 팔로우 API에 언팔로우 기능 추가

--- src/routes/follows.router.js
+++ src/routes/follows.router.js
@@ -14,6 +14,20 @@ router.post(
       const followerId = req.user.userId;
 
       if (!followingId) {
+        return res.status(404).json({
+          success: false,
+          message: "팔로우하려는 유저를 지정해주세요.",
+        });
+      }
+
+      const followingUser = await prisma.users.findFirst({
+        where: { userId: +followingId },
+        select: {
+          nickname: true,
+        },
+      });
+
+      if (!followingUser) {
         return res
           .status(404)
           .json({ success: false, message: "존재하지 않는 유저입니다." });
@@ -24,29 +38,30 @@ router.post(
           success: false,
           message: "스스로를 팔로우할 수는 없습니다.",
         });
-      } // 이거 필요할까요?
+      }
 
       const isExistingFollowing = await prisma.follows.findFirst({
         where: { followerId: +followerId, followingId: +followingId },
+        select: { followId: true },
       });
-
+      //이미 팔로우 한 사람이면 언팔로우
       if (isExistingFollowing) {
-        return res
-          .status(409)
-          .json({ success: false, message: "이미 팔로우 한 유저입니다." });
+        await prisma.follows.delete({
+          where: {
+            followId: isExistingFollowing.followId,
+          },
+        });
+
+        return res.status(201).json({
+          success: true,
+          message: `${followingUser.nickname}님을 언팔로우했습니다.`,
+        });
       }
 
       const follow = await prisma.follows.create({
         data: {
           followingId: +followingId,
           followerId: +followerId,
-        },
-      });
-
-      const followingUser = await prisma.users.findFirst({
-        where: { userId: +followingId },
-        select: {
-          nickname: true,
         },
       });
 
@@ -59,5 +74,61 @@ router.post(
     }
   }
 );
+
+// 언팔로우 API
+// router.delete(
+//   "/users/:userId/follows",
+//   authMiddleware,
+//   async (req, res, next) => {
+//     try {
+//       const followingId = req.params.userId;
+//       const followerId = req.user.userId;
+
+//       if (!followingId) {
+//         return res.status(404).json({
+//           success: false,
+//           message: "언팔로우하려는 유저를 지정해주세요.",
+//         });
+//       }
+
+//       const followingUser = await prisma.users.findFirst({
+//         where: { userId: +followingId },
+//         select: {
+//           nickname: true,
+//         },
+//       });
+
+//       if (!followingUser) {
+//         return res
+//           .status(404)
+//           .json({ success: false, message: "존재하지 않는 유저입니다." });
+//       }
+
+//       const Follow = await prisma.follows.findFirst({
+//         where: { followerId: +followerId, followingId: +followingId },
+//         select: { followId: true },
+//       });
+
+//       if (!Follow) {
+//         return res
+//           .status(404)
+//           .json({ success: false, message: "팔로우한 적 없는 유저입니다." });
+//       }
+
+//       const unfollow = await prisma.follows.delete({
+//         where: {
+//           followId: Follow.followId,
+//         },
+//       });
+
+//       return res.status(201).json({
+//         success: true,
+//         message: `${followingUser.nickname}님을 언팔로우하였습니다.`,
+//       });
+//     } catch (err) {
+//       next(err);
+//     }
+//   }
+// );
 
 export default router;
